2236c00bde4884097c4e6bdb0ca89d75216484da
Allan <allanpaiva17@gmail.com>
2022-11-09 11:15:22 -0300

    logs with scan

--- hello.go
+++ hello.go
@@ -7,13 +7,18 @@ import (
 
 func main() {
 	nome := "Alan"
-	idade := 24
-	var altura = 1.75
 	var versao float32 = 1.1
-
-	fmt.Println("olá senhor", nome, "sua idade é:", idade, "com a altura de:", altura)
+	fmt.Println("olá senhor", nome, "sua idade é:")
 	fmt.Println("versão atual:", versao)
 
 	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
 
+	fmt.Println("1- Iniciar Monitoramento dos Sites")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("o comando escolhido foi", comando)
 }
